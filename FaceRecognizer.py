956dba05bee992bed9c33f7632d89aed4fb08694
azeharia <aviv_zeharia_tp@bmc.com>
2022-02-25 14:15:08 +0200

    some improve

--- FaceRecognizer.py
+++ FaceRecognizer.py
@@ -17,9 +17,9 @@ class FaceReconizer():
         self.recognized = []
         self.numOfFaces = 0
         self.tolerance = 0.6
-        self.frame_resizing = 0.3
-        self.t = time.time()
+        self.frame_resizing = 0.25
         self.face_locations = []
+        self.difference = 200
 
     def load_data(self):
         """
@@ -69,13 +69,25 @@ class FaceReconizer():
 
         print("Encoding images loaded")
 
+    def getFacesLocations(self, img):
+        small_frame = cv2.resize(img, (0, 0), fx=self.frame_resizing, fy=self.frame_resizing)
+        rgb_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
+        face_locations = face_recognition.face_locations(rgb_frame)
+        face_locations = np.array(face_locations)
+        face_locations = face_locations / self.frame_resizing
+        return face_locations.astype(int)
+
     def findFaces(self, img):
 
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.faceDetection.process(imgRGB)
-        # print(self.results)
         bboxs = []
-        cnt = 0
+
+        # Find all the faces and face encodings in the current frame of video
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+
+        face_locations = self.getFacesLocations(img)
+
         if self.results.detections:
             for id, detection in enumerate(self.results.detections):
                 print(self.numOfFaces)
@@ -88,8 +100,7 @@ class FaceReconizer():
                 img = self.fancyDraw(img, bbox)
                 bboxs.append([y, x1, y1, x])
 
-
-            if self.needToRecognizeAgain(self.sort_locations(self.face_locations), self.sort_locations(bboxs)):
+            if self.needToRecognizeAgain(self.sort_locations(self.face_locations), self.sort_locations(face_locations)):
                 self.recognizeFaces(img)
 
             for bbox in bboxs:
@@ -97,33 +108,38 @@ class FaceReconizer():
                 print(name)
                 print(self.recognized)
                 cv2.putText(img, f'{name} {int(detection.score[0] * 100)}%',
-                            (bbox[1], bbox[2] - 10), cv2.FONT_HERSHEY_PLAIN,
+                            (bbox[3], bbox[0] - 10), cv2.FONT_HERSHEY_PLAIN,
                             2, (255, 0, 255), 2)
 
         return img, bboxs
 
     def findName(self, bbox):
-        print('findname', bbox)
+        print('findName', bbox)
         if len(self.recognized) == 0 or len(self.face_locations) == 0:
             return ""
         distances = []
         for v in self.face_locations:
             distances.append(np.linalg.norm(v - bbox))
 
-        print('finName' , min(distances))
-        if min(distances) > 200:
+        print('findName', min(distances))
+        if min(distances) > self.difference:
             return 'Unknown'
         return self.recognized[np.argmin(distances)]
 
     def sort_locations(self, locations):
+        """
+
+        :param locations: the vectors to sort
+        :return: list of vectors sorted by size of vector
+        """
         sizes = []
-        for loc in locations:
+        for loc in locations:  # calculate the size of each vector
             sum = np.square(loc[0]) + np.square(loc[1]) + np.square(loc[2]) + np.square(loc[3])
             size = np.sqrt(sum)
             sizes.append((loc, size))
 
         lst = len(sizes)
-        for i in range(0, lst):
+        for i in range(0, lst):  # bubble sort by size
             for j in range(0, lst - i - 1):
                 if (sizes[j][1] > sizes[j + 1][1]):
                     temp = sizes[j]
@@ -132,6 +148,14 @@ class FaceReconizer():
         return [tup[0] for tup in sizes]
 
     def needToRecognizeAgain(self, V, B):
+        """
+
+        :param V: list of vectors
+        :param B: list of vectors
+        :return: True if the distance between vi to bi is greater than self.difference
+        """
+        if len(V) != len(B):
+            return True
         print(V)
         print(B)
 
@@ -140,7 +164,7 @@ class FaceReconizer():
         dist = -1
         for v, b in zip(V, B):
             dist = np.linalg.norm(v - b)
-            if dist > 200:
+            if dist > self.difference:
                 print('needToRecognizeAgain', dist)
                 return True
         print('needToRecognizeAgain', dist)
@@ -152,14 +176,8 @@ class FaceReconizer():
         # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
         rgb_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
         face_locations = face_recognition.face_locations(rgb_frame)
-
-        # if self.numOfFaces == len(face_locations):
-        #     return self.recognized
-        # self.recognized.clear()
-        # self.numOfFaces = len(face_locations)
-        self.recognized.clear()
-
         face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
+        self.recognized.clear()
 
         for face_encoding in face_encodings:
             # See if the face is a match for the known face(s)
@@ -178,7 +196,7 @@ class FaceReconizer():
         face_locations = np.array(face_locations)
         face_locations = face_locations / self.frame_resizing
         self.face_locations = face_locations.astype(int)
-        print(self.face_locations)
+        print('recognizeFaces', self.face_locations)
 
     def fancyDraw(self, img, bbox, l=30, t=5, rt=1):
         x, y, w, h = bbox
