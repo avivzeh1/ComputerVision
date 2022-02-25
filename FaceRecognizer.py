e02afdc67833247329cc334694afa787580ddde7
azeharia <aviv_zeharia_tp@bmc.com>
2022-02-25 22:37:11 +0200

    add decipher picture option

--- FaceRecognizer.py
+++ FaceRecognizer.py
@@ -9,11 +9,10 @@ class FaceReconizer():
         self.known_face_encodings = []
         self.known_face_names = []
         self.recognized = []
-        self.numOfFaces = 0
         self.tolerance = 0.6
-        self.frame_resizing = 0.25
+        self.frame_resizing = 0.25  # resizing the image for faster performance
         self.face_locations = []
-        self.difference = 240
+        self.difference = 250
 
     def load_data(self):
         """
@@ -22,13 +21,8 @@ class FaceReconizer():
         images_path = glob.glob(os.path.join('images/', "**/*.npy"))
         print("{} encoding images found.".format(len(images_path)))
 
-        # Store image encoding and names
         for img_path in images_path:
-            # Get the name only from the initial file path.
-
             filename = img_path.split('\\')[1]
-            # Get encoding
-
             with open(img_path, 'rb') as f:
                 img_encoding = np.load(f)
 
@@ -53,23 +47,26 @@ class FaceReconizer():
         for img_path in images_path:
             img = cv2.imread(img_path)
             rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
             filepath = img_path.split('.')[0]
-            # Get encoding
-            img_encoding = face_recognition.face_encodings(rgb_img)[0]
-            # save the image data to file
+            img_encoding = face_recognition.face_encodings(rgb_img)[0]  # Get encoding
+            # save the image data to numpy file
             with open(filepath + 'Data.npy', 'wb') as f:
                 np.save(f, img_encoding)
 
         print("Encoding images loaded")
 
-    def getFacesLocations(self, img):
-        small_frame = cv2.resize(img, (0, 0), fx=self.frame_resizing, fy=self.frame_resizing)
+    def getFacesLocations(self, img, resizing=True):
+        if resizing:
+            small_frame = cv2.resize(img, (0, 0), fx=self.frame_resizing, fy=self.frame_resizing)
+        else:
+            small_frame = cv2.resize(img, (0, 0), fx=1, fy=1)
+
         rgb_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
         face_locations = face_recognition.face_locations(rgb_frame)
         face_locations = np.array(face_locations)
-        face_locations = face_locations / self.frame_resizing
-        return face_locations.astype(int)
+        if resizing:
+            face_locations = face_locations / self.frame_resizing
+        return list(face_locations.astype(int))
 
     def findFaces(self, img):
         # Find all the faces and face encodings in the current frame of video
@@ -78,32 +75,29 @@ class FaceReconizer():
         face_locations = self.getFacesLocations(img)
 
         for loc in face_locations:
-            print(self.numOfFaces)
-
             img = self.fancyDraw(img, loc)
-
 
         if self.needToRecognizeAgain(self.sort_locations(self.face_locations), self.sort_locations(face_locations)):
             self.recognizeFaces(img)
 
         for loc in face_locations:
             name = self.findName(loc)
-            print(name)
+            # print(name)
             print(self.recognized)
             cv2.putText(img, f'{name}', (loc[3], loc[0] - 10), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 2)
 
         return img
 
     def findName(self, bbox):
-        print('findName', bbox)
+        #print('findName', bbox)
         if len(self.recognized) == 0 or len(self.face_locations) == 0:
             return ""
         distances = []
         for v in self.face_locations:
             distances.append(np.linalg.norm(v - bbox))
 
-        print('findName', min(distances))
-        if min(distances) > self.difference:
+        #print('findName', min(distances))
+        if min(distances) > self.difference:  # too far
             return 'Unknown'
         return self.recognized[np.argmin(distances)]
 
@@ -137,8 +131,8 @@ class FaceReconizer():
         """
         if len(V) != len(B):
             return True
-        print(V)
-        print(B)
+        # print(V)
+        # print(B)
 
         if len(V) == 0:
             return True
@@ -146,13 +140,17 @@ class FaceReconizer():
         for v, b in zip(V, B):
             dist = np.linalg.norm(v - b)
             if dist > self.difference:
-                print('needToRecognizeAgain', dist)
+                # print('needToRecognizeAgain', dist)
                 return True
-        print('needToRecognizeAgain', dist)
+        # print('needToRecognizeAgain', dist)
         return False
 
-    def recognizeFaces(self, img):
-        small_frame = cv2.resize(img, (0, 0), fx=self.frame_resizing, fy=self.frame_resizing)
+    def recognizeFaces(self, img, resizing=True):
+        if resizing:
+            small_frame = cv2.resize(img, (0, 0), fx=self.frame_resizing, fy=self.frame_resizing)
+        else:
+            small_frame = cv2.resize(img, (0, 0), fx=1, fy=1)
+
         # Find all the faces and face encodings in the current frame of video
         # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
         rgb_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
@@ -161,11 +159,10 @@ class FaceReconizer():
         self.recognized.clear()
 
         for face_encoding in face_encodings:
-            # See if the face is a match for the known face(s)
-            # matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
-            # get the distances between faces from data to face from the video
+            # See if the face is a match for the known faces
+            # get the distances between faces from data to face from the input
             face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
-            best_match_index = np.argmin(face_distances)  # best match found
+            best_match_index = np.argmin(face_distances)  # best match found, minimum distance
 
             if face_distances[best_match_index] <= self.tolerance:
                 name = self.known_face_names[best_match_index]  # get the name of this match
@@ -174,27 +171,44 @@ class FaceReconizer():
                 name = 'Unknown'
                 p = 0
             self.recognized.append(name + ' ' + str(p) + '%')
-            print(f'{name} {p}%')
+            #print(f'{name} {p}%')
         face_locations = np.array(face_locations)
-        face_locations = face_locations / self.frame_resizing
-        self.face_locations = face_locations.astype(int)
-        print('recognizeFaces', self.face_locations)
+        if resizing:
+            face_locations = face_locations / self.frame_resizing
+        self.face_locations = list(face_locations.astype(int))
+        #print('recognizeFaces', self.face_locations)
 
     def fancyDraw(self, img, bbox, l=30, t=5):
         y1, x2, y2, x1 = bbox[0], bbox[1], bbox[2], bbox[3]
 
         cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 200), 4)
-        #Top Left  x,y
+        #Top Left  x1,y1
         cv2.line(img, (x1, y1), (x1 + l, y1), (255, 0, 255), t)
         cv2.line(img, (x1, y1), (x1, y1 + l), (255, 0, 255), t)
-        # Top Right  x1,y
+        # Top Right  x2,y1
         cv2.line(img, (x2, y1), (x2 - l, y1), (255, 0, 255), t)
         cv2.line(img, (x2, y1), (x2, y1 + l), (255, 0, 255), t)
-        # Bottom Left  x,y1
+        # Bottom Right  x2,y2
         cv2.line(img, (x2, y2), (x2 - l, y2), (255, 0, 255), t)
         cv2.line(img, (x2, y2), (x2, y2 - l), (255, 0, 255), t)
-        #Bottom Right  x1,y1
+        #Bottom Left  x1,y2
         cv2.line(img, (x1, y2), (x1 + l, y2), (255, 0, 255), t)
         cv2.line(img, (x1, y2), (x1, y2 - l), (255, 0, 255), t)
         return img
+
+    def imageRecognizer(self, path):
+        img = cv2.imread(path)
+        self.recognizeFaces(img, False)  # no resizing for more accuracy
+        for loc in self.face_locations:
+            img = self.fancyDraw(img, loc)
+            name = self.findName(loc)
+            print(name)
+            print(self.recognized)
+            cv2.putText(img, f'{name}', (loc[3], loc[0] - 10), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 2)
+
+        self.recognized.clear()
+        self.face_locations.clear()
+
+        cv2.imshow("Deciphered image", img)
+
 
--- main.py
+++ main.py
@@ -218,53 +218,74 @@ def filter_mask(mask):
 
     return dilation
 
-
+def userInteraction():
+    answer = input('Do you want to decipher an image? if you want to decipher live video choose No Y/N: ')
+    if answer.lower() == 'y':
+        answer = input('Enter the path of the image: ')
+        return answer
+    return 'n'
 
 def main():
     cap = cv2.VideoCapture(0)
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1000)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1000)
-    # sfr.save_encoding_images("images/")  #run only once
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1100)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1100)
 
-    print('please show the background now')
-    _, background = cap.read()
-    time.sleep(4)
-    _, background = cap.read()
-    pTime = 0
-    detector = FaceReconizer()
-    # sfr.save_encoding_images("images/")  #run only once
+    recognizer = FaceReconizer()
+    #recognizer.save_encoding_images("images/")  #run only once if there are new images
     print('Loading images from database...')
-    detector.load_data()
+    recognizer.load_data()
+    path = userInteraction()
+    if path != 'n':
+        try:
+            recognizer.imageRecognizer(path)
+            print('Type q to quit...')
+            while cv2.waitKey(1) != ord('q'):
+                continue
+            return
+        except:
+            print('Image does not exist or path is wrong')
+
+    answer = input('Do you want the invisible cloak effect? Y/N: ')
+    invisible_cloack = False
+    if answer.lower() == 'y':
+        input('Please confirm that you are ready to show the background now, once you confirmed you have 4 seconds: ')
+        for i in range(1, 5):
+            _, background = cap.read()
+            time.sleep(1)
+            print(i, '...')
+        _, background = cap.read()
+        invisible_cloack = True
     while True:
         success, img = cap.read()
+        combined = img
+        if invisible_cloack:
+            hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+            # lower bound and upper bound for Green color
+            lower_bound = np.array([50, 80, 50])
+            upper_bound = np.array([90, 255, 255])
 
-        # lower bound and upper bound for Green color
-        lower_bound = np.array([50, 80, 50])
-        upper_bound = np.array([90, 255, 255])
+            # find the colors within the boundaries
+            mask = cv2.inRange(hsv, lower_bound, upper_bound)
 
-        # find the colors within the boundaries
-        mask = cv2.inRange(hsv, lower_bound, upper_bound)
+            # Filter mask
+            mask = filter_mask(mask)
 
-        # Filter mask
-        mask = filter_mask(mask)
+            # Apply the mask to take only those region from the saved background
+            # where our cloak is present in the current frame
+            cloak = cv2.bitwise_and(background, background, mask=mask)
 
-        # Apply the mask to take only those region from the saved background
-        # where our cloak is present in the current frame
-        cloak = cv2.bitwise_and(background, background, mask=mask)
+            # create inverse mask
+            inverse_mask = cv2.bitwise_not(mask)
 
-        # create inverse mask
-        inverse_mask = cv2.bitwise_not(mask)
+            # Apply the inverse mask to take those region of the current frame where cloak is not present
+            current_background = cv2.bitwise_and(img, img, mask=inverse_mask)
 
-        # Apply the inverse mask to take those region of the current frame where cloak is not present
-        current_background = cv2.bitwise_and(img, img, mask=inverse_mask)
+            # Combine cloak region and current_background region to get final frame
+            combined = cv2.add(cloak, current_background)
 
-        # Combine cloak region and current_background region to get final frame
-        combined = cv2.add(cloak, current_background)
-
-        img = detector.findFaces(combined)
-        cv2.imshow("Invisible Cloak", combined)
+        img = recognizer.findFaces(combined)
+        cv2.imshow("Live cam", combined)
 
         if cv2.waitKey(1) == ord('q'):
             break
